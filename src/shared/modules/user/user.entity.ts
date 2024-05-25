d8cd6ca82fc3b7ca65aeb1954a6477478ff40522
@ext.malistarova <ext.malistarova@tinkoff.ru>
2024-05-25 20:39:33 +0300

    fix: Фиксит user.entity.ts

--- src/shared/modules/user/user.entity.ts
+++ src/shared/modules/user/user.entity.ts
@@ -3,13 +3,11 @@ import {
   getModelForClass,
   prop,
   modelOptions,
-  Ref,
 } from '@typegoose/typegoose';
 
 import {User} from '../../types/index.js';
 import {UserType} from '../../enum/index.js';
 import {createSHA256} from '../../helpers/index.js';
-import { OfferEntity } from '../offer/offer.entity.js';
 
 // eslint-disable-next-line @typescript-eslint/no-unsafe-declaration-merging
 export interface UserEntity extends defaultClasses.Base {}
@@ -40,14 +38,6 @@ export class UserEntity extends defaultClasses.TimeStamps implements User {
 
   @prop({required: false, default: ''})
   public avatar?: string;
-
-  @prop ({
-    required: false,
-    ref: OfferEntity,
-    default: [],
-    _id: false
-  })
-  public favoriteOffers!: Ref<OfferEntity>[];
 
   @prop({
     required: true,
