7233a7b57e42b5534833aac1b962772c40b3e825
@ext.malistarova <ext.malistarova@tinkoff.ru>
2024-06-17 09:32:04 +0300

    chore: Ошибки линтера'

--- src/shared/libs/rest/service/service.abstract.ts
+++ src/shared/libs/rest/service/service.abstract.ts
@@ -5,7 +5,7 @@ import {ServiceInterface} from './service.interface.js';
 
 @injectable()
 export abstract class AbstractService<T> implements ServiceInterface<T> {
-  private readonly abstactModel: types.ModelType<DocumentType<T>>
+  private readonly abstactModel: types.ModelType<DocumentType<T>>;
 
   public async findById(documentId: string): Promise<DocumentType<T> | null> {
     return this.abstactModel.findById(documentId);
